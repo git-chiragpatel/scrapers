f6d0b843f08397e2300bab77a712b1ca10b13e6e
git-chiragpatel <127727500+git-chiragpatel@users.noreply.github.com>
2023-03-20 13:28:17 +0530

    done

--- scraper/unom.py
+++ scraper/unom.py
@@ -1,24 +1,34 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
-driver.get("https://www.unom.ac.in/index.php?route=administration/announcement")
+driver_service = Service('chromedriver.exe')
+driver = webdriver.Chrome(service=driver_service)
 
-driver.implicitly_wait(10)
-page_source = driver.page_source
+success = []
+failure = []
+scrapers_report = []
+news_articles = []
 
-soup = BeautifulSoup(page_source, 'html.parser')
-
-# Find the news section
-news_section = soup.find('div', {'class': 'blog'})
-
-# Extract the news items
-news_items = news_section.find_all('div', {'class': 'media'})
-
-for item in news_items:
-    title = item.find('h4', {'class': 'media-heading'}).text.strip()
-    date = item.find('span', {'class': 'date'}).text.strip()
-    print(f'{title} ({date})\n')
-    
-driver.quit()
+try:
+    url = "https://www.unom.ac.in/index.php?route=administration/announcement"
+    base_url = "https://www.unom.ac.in/"
+    name = 'University of Madras'
+    scrapers_report.append([url,base_url,name])
+    driver.get(url)
+    time.sleep(3)
+    results = driver.find_elements(By.CLASS_NAME,"media-body")
+    for i in results:
+        headline = i.find_element(By.TAG_NAME , "a").text
+        link = i.find_element(By.TAG_NAME , "a").get_attribute('href')
+        if "http" not in link:
+            link = base_url + link
+        news_articles.append(("University of Madras",headline,link))
+    success.append('University of Madras')
+    driver.quit()
+except Exception as e:
+    driver.quit()
+    failure.append((name,e))
+    pass
